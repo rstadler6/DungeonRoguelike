4de8f9c61b3114501373e2ca34d73c5d31f492a5
Raphael Stadler <stadlrap@students.zhaw.ch>
2026-03-24 11:16:36 +0100

    add gum ui

--- Progression/XpLevel.cs
+++ Progression/XpLevel.cs
@@ -2,5 +2,22 @@ namespace DungeonRoguelike.Progression;
 
 public class XpLevel
 {
+    public int Level { get; private set; } = 1;
+    public int Experience { get; private set; } = 0;
     
+    public void CollectExperience(int amount)
+    {
+        Experience += amount;
+        CheckLevelUp();
+    }
+
+    private void CheckLevelUp()
+    {
+        if (Experience >= Level * 1.2 * 100)
+        {
+            Level++;
+            Experience = 0;
+        }
+        // TODO level up action
+    }
 }
